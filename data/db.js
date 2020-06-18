7ebef2d285ca064cd9dd376476c3ee980243d9c8
tomgreg90 <tomgreg999@btinternet.com>
2020-06-18 11:42:40 +0100

    set database url

--- data/db.js
+++ data/db.js
@@ -1,10 +1,17 @@
 const Sequelize = require("sequelize");
 const musicianData = require("./musicians");
 
-const db = new Sequelize("musicians", "tomgreg90", "password", {
-  dialect: "postgres",
-  storage: "localhost",
-});
+let db;
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+  });
+} else {
+  db = new Sequelize("musicians", "tomgreg90", "password", {
+    dialect: "postgres",
+    storage: "localhost",
+  });
+}
 
 const Musician = db.define("musician", {
   firstName: { type: Sequelize.STRING },
